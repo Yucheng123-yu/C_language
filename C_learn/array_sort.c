8893c6b6d32418e6ed7a3e4538d3182e3fd2c1c6
Yucheng123-yu <2213303211@qq.com>
2024-09-15 20:28:12 +0800

    Initial creation

---
+++ C_learn/array_sort.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int array[] = {3,1,4,9,1,5,2,6,9};
+int size = sizeof(array)/sizeof(array[0]);
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+void print_array(int array[],int low,int high)
+{
+    for(int i=low;i<high;i++)
+    {
+        printf("%d ",array[i]);
+    }
+    printf("\n");
+}
+void subsort(int array[],int low,int high) 
+{
+    if(low<high)
+    {
+        int pref = low;
+        int i = low + 1;
+        int j = low + 1;
+        while(j<=high){
+            if(array[j]<array[pref])
+            {
+                swap(array,i,j);
+                print_array(array, low, high);
+                i++;
+            }
+            j++;
+        }
+        swap(array,low,i-1);
+        printf("a---");
+        print_array(array, low, high);
+        subsort(array,low,i-2);
+        subsort(array,i,high);
+    }
+    
+}
+
+int main()
+{
+    
+    print_array(array, 0 , size);
+    // swap(array,1,5);
+    // print_array(array,size);
+    subsort(array,0,size-1);
+    print_array(array, 0, size);
+    return 0;
+}
---
+++ C_learn/node.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} snode;
+
+void scanf_node(snode **head, int len)
+{// Scanf the node
+    snode *p, *q;
+    for (int i = 0; i < len; i++)
+    {
+        p = (snode *)malloc(sizeof(snode));
+        scanf("%d", &p->data);
+        p->next = NULL;
+        if (*head == NULL)
+            *head = p;
+        else
+            q->next = p;
+        q = p;
+    }
+}
+void add_node(snode **head,int data)
+{// Add node to the end
+    snode *p, *q;
+    if(*head == NULL)
+    {
+        p = (snode *)malloc(sizeof(snode));
+        p->data = data;
+        p->next = NULL;
+        *head = p;
+    }
+    else{
+        p = (snode *)malloc(sizeof(snode));
+        p->data = data;
+        p->next = NULL;
+        q = *head;
+        while (q->next != NULL)
+        {
+            q = q->next;
+        }
+        q->next = p;
+    }
+}
+void print_node(snode *head)
+{// Print the node
+    snode *p;
+    p = head;
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+void insert_node(snode **head, int th, int data)
+{// Insert node at th position
+    if (*head == NULL || th < 1)
+        return;
+
+    snode *p, *q;
+    if (th == 1)
+    {
+        p = (snode *)malloc(sizeof(snode));
+        p->data = data;
+        p->next = *head;
+        *head = p;
+        return;
+    }
+    p = *head;
+    for (int i = 0; i < th - 1 && p != NULL; i++)
+    {
+        q = p;
+        p = p->next;
+    }
+
+    if (p == NULL)
+        return;
+    // printf("Out of range\n");
+    q->next = p->next;
+    snode *new = (snode *)malloc(sizeof(snode));
+    new->data = data;
+    new->next = p;
+    q->next = new;
+}
+
+int  calc_length(snode *head)
+{// Calculate the length of the list
+    snode *p;
+    int len = 0;
+    p = head;
+    while (p != NULL)
+    {
+        len++;
+        p = p->next;
+    }
+    // printf("Length of the list is %d\n", len);
+    return len;
+}
+
+void array_to_nodelist(snode **head, int *arr, int len)
+{// Convert array to node list
+    snode *p, *q;
+    if(*head != NULL)
+    {
+        q = *head;
+        while (q->next != NULL)
+        {
+            q = q->next;
+        }
+        for (size_t i = 0; i < len; i++)
+        {
+            p = (snode *)malloc(sizeof(snode));
+            p->data = arr[i];
+            p->next = NULL;
+            q->next = p;
+            q = p;
+        }
+        return;
+    }
+    for (int i = 0; i < len; i++)
+    {
+
+        snode *p = (snode *)malloc(sizeof(snode));
+        p->data = arr[i];
+        p->next = NULL;
+        if (*head == NULL)
+        {
+            *head = p;
+        }
+        else
+        {
+            q->next = p;
+        }
+        q = p;
+    }
+}
+void swapnode(snode *a, snode *b)
+{
+    int temp = a->data;
+    a->data = b->data;
+    b->data = temp;
+}
+void sort_nodelist(snode *low,snode *high)
+{// Sort the node list
+    if(low->next == NULL || low == NULL || low == high)
+        return;
+    int pvef = low->data;
+    snode *i_pre = low;
+    snode *i = low->next;
+    snode *j = low->next;
+
+    while(j->next != NULL)
+    {
+        if(j->data < pvef)
+        {
+            swapnode(i, j);
+            i_pre = i;
+            i = i->next;
+        }
+        
+        j= j->next;
+    }
+    swapnode(low, i_pre);
+    sort_nodelist(low, i_pre);
+    sort_nodelist(i, high);
+}
+
+int main()
+{
+    //    snode *head;
+    //    head = NULL;
+    //    int len;
+    //    printf("Enter the length of the list: ");
+    //    scanf("%d", &len);
+    //    scanf_node(&head, len);
+    //    print_node(head);
+
+    snode *head1;
+    head1 = NULL;
+    int arr[] = {5, 2, 1, 4, 9, 6, 2, 8, 9};
+
+    array_to_nodelist(&head1, arr, 9);
+    print_node(head1);
+
+    // add_node(&head1, 10);
+    // print_node(head1);
+
+    
+    // calc_length(head1);
+    // insert_node(&head1, 3, 99);
+    // print_node(head1);
+    // insert_node(&head1, 11, 99);
+    // print_node(head1);
+
+    int arr1[] = {51, 12, 33, 24, 55, 36, 76, 26, 96};
+    array_to_nodelist(&head1, arr1, 9);
+    print_node(head1);
+
+    printf("High is %d\n", head1->data);
+
+    snode *head_high;
+    head_high = head1;
+    // int num = 0;
+    while(head_high->next != NULL){
+        head_high = head_high->next;
+        // num++;
+    }
+    // printf("Num is %d\n", num);
+    // printf("High is %d\n", head_high->data);
+    sort_nodelist(head1, head_high);
+    
+    print_node(head1);
+
+    return 0;
+}
